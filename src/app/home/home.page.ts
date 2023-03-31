e323b7497cb9b02f08ca9be5c359fe97583187f4
json-alzate <developer.alzate@gmail.com>
2023-03-31 15:19:23 -0500

    🎉

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,95 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+import * as tf from '@tensorflow/tfjs';
+import * as tmImage from '@teachablemachine/image';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+
+  loading = false;
+
+  urlImg!: string | undefined;
+
+  model!: tmImage.CustomMobileNet;
+  maxPredictions!: number;
+  predictionResult!: string;
+
+  constructor() { }
+
+
+  async takePicture() {
+    this.loading = true;
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+    });
+
+    const imageUrl = image.base64String as string;
+    this.urlImg = imageUrl;
+
+    // Load model if it's not loaded yet
+    if (!this.model) {
+      await this.loadModel();
+    }
+
+    // Predict the label
+    this.predictionResult = await this.predict(imageUrl);
+    this.loading = false;
+  }
+
+
+
+  async loadModel() {
+    const modelURL = '/assets/tm-my-image-model/model.json';
+    const metadataURL = '/assets/tm-my-image-model/metadata.json';
+
+    this.model = await tmImage.load(modelURL, metadataURL);
+    this.maxPredictions = this.model.getTotalClasses();
+  }
+
+
+  async predict(base64Image: string) {
+
+
+    // Convert base64 image to HTMLImageElement
+    const image = new Image();
+    image.src = 'data:image/jpeg;base64,' + base64Image;
+
+    await new Promise((resolve) => (image.onload = resolve));
+
+    // Run prediction
+    const prediction = await this.model.predict(image);
+
+    // Find the prediction with the highest probability
+    let highestProbability = -1;
+    let bestClassName = '';
+    for (let i = 0; i < this.maxPredictions; i++) {
+      if (prediction[i].probability > highestProbability) {
+        highestProbability = prediction[i].probability;
+        bestClassName = prediction[i].className;
+      }
+    }
+
+    return bestClassName;
+  }
+
+
+
+
+
 }
